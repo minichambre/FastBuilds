ab14e3f072fb716c2a1d9c30e48dd8d1c1fc91ff
Dan <minichambre95@gmail.com>
2020-11-08 13:28:46 +0000

    Added public layout

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
- 
-// Connection URL
-//const url = 'mongodb://localhost:27017';
-const url = process.env.MONGO_CONNECTION_STRING;
 
+// Connection URL
+const url = process.env.MONGO_CONNECTION_STRING;
 // Database Name
 const dbName = 'fastbuilds';
- 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+setTimeout(() => {
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
 
+        const db = client.db(dbName);
+
+        client.close();
+    });
+},5000)
+
+//Start express server
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+//Homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
   })
   
