c08cd3eaa4850699cdc651afce2474af8cfd0af2
Dan <minichambre95@gmail.com>
2020-11-08 15:53:49 +0000

    Updated styling

--- src/js/components/SearchForm/index.js
+++ src/js/components/SearchForm/index.js
@@ -5,11 +5,11 @@ export default class SearchForm extends Component {
     constructor() {
         super();
         this.state = {
-            value: ''
+            value: '',
+            results: []
         }
         this.handleInput = this.handleInput.bind(this);
         this.lookup = this.lookup.bind(this);
-        this.parse = this.parse.bind(this);
     }
 
     handleInput(event) {
@@ -27,26 +27,37 @@ export default class SearchForm extends Component {
             if (response.status !== 200) {
                 console.log('error');
             }
-            response.json().then(asJson => this.parse(asJson));
+            response.json().then(asJson => this.setState({results: asJson}));
         })
         .catch(err => {
             console.log(err);
         })
     }
 
-    parse(championData) {
-        console.log(championData)
-    }
 
     render() {
         return (
-            <input 
-                className='championSearch' 
-                type='text' 
-                value={this.state.value} 
-                placeholder='Singed Mid... Braum Jungle...' 
-                onChange={this.handleInput}
-            />
+            <div className='searchWrap'>
+                <input 
+                    className='championSearch font-body' 
+                    type='text' 
+                    value={this.state.value} 
+                    placeholder='Singed Mid... Braum Jungle...' 
+                    onChange={this.handleInput}
+                />
+                {this.state.results.length > 0 &&
+                    <div className='resultList'>
+                    {this.state.results.map((champion, index) => {
+                        return (
+                            <div className='resultItem font-body font-bold'>
+                                {champion.name}
+                            </div>
+                        )
+
+                    })}
+                    </div>
+                }
+            </div>
         )
     }
 }
